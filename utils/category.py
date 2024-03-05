aa753cf03ca505f0be173b037b7786b90afd22c8
Sergey Atkov <bersercwow@gmail.com>
2024-03-05 15:47:35 +0700

    change variable name

--- utils/category.py
+++ utils/category.py
@@ -7,7 +7,7 @@ class Category:
     goods: list
 
     total_number_cat = 0
-    total_number_unic_cat = 0
+    total_number_unic_prod = 0
 
     def __init__(self, name: str, description: str, goods: list):
         self.name = name
