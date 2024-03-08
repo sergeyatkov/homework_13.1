976ed00a48c4e4da9d3cac64077ec753129d6880
Sergey Atkov <bersercwow@gmail.com>
2024-03-08 21:23:52 +0700

    complete task 1, 2, 3.

--- utils/category.py
+++ utils/category.py
@@ -7,7 +7,7 @@ class Category:
     """
     name: str
     description: str
-    products: list[Product]
+    __products: list[Product]
 
     total_number_cat = 0
     total_number_unic_prod = 0
@@ -15,7 +15,17 @@ class Category:
     def __init__(self, name: str, description: str, products: list[Product]):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.total_number_cat += 1
         Category.total_number_unic_prod += len(products)
+
+    @property
+    def get_products(self) -> list[Product]:
+        return self.__products
+
+    def add_product(self, product: [Product]):
+        self.__products.append(product)
+
+    def new_product(self, param, param1, param2, param3):
+        pass
--- utils/product.py
+++ utils/product.py
@@ -12,3 +12,13 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @property
+    def get_info(self):
+        return (f"{self.name}, {self.price} руб. "
+                f"Остаток: {self.quantity} шт.")
+
+    @classmethod
+    def new_product(cls, name: str, description: str, price: float, quantity: int):
+        cls.name, cls.description, cls.price, cls.quantity = input().split()
+        return cls(name, description, price, quantity)
